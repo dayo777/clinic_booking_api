963161b81b10ca2fb43a58d0a7bc67af6a70af76
dayo777 <oruns@outlook.com>
2025-12-31 16:17:16 +0100

    modified failing test handler

--- clinic_core/tests/handler_tests.rs
+++ clinic_core/tests/handler_tests.rs
@@ -1,11 +1,9 @@
-use actix_web::{test, App};
+use actix_web::{App, test};
 use appointment_service::appointment_config_v1;
 use clinic_service::clinic_config_v1;
 #[actix_web::test]
 async fn test_appointment_get() {
-    let app = test::init_service(
-        App::new().configure(appointment_config_v1)
-    ).await;
+    let app = test::init_service(App::new().configure(appointment_config_v1)).await;
     let req = test::TestRequest::get().uri("/appointments").to_request();
     let resp = test::call_service(&app, req).await;
     assert!(resp.status().is_success());
@@ -13,9 +11,7 @@ async fn test_appointment_get() {
 
 #[actix_web::test]
 async fn test_clinic_get() {
-    let app = test::init_service(
-        App::new().configure(clinic_config_v1)
-    ).await;
+    let app = test::init_service(App::new().configure(clinic_config_v1)).await;
     let req = test::TestRequest::get().uri("/clinic").to_request();
     let resp = test::call_service(&app, req).await;
     assert!(resp.status().is_success());

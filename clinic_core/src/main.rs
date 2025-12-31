ef4cc4f992bdd6016ff2fad7982c6310cf78d4e9
dayo777 <oruns@outlook.com>
2025-12-31 11:37:08 +0100

    WIP: Auth feature progress

--- clinic_core/src/main.rs
+++ clinic_core/src/main.rs
@@ -2,7 +2,11 @@ use actix_web::{App, HttpServer, Responder, web};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(hello)))
+    HttpServer::new(|| {
+        App::new()
+             .route("/", web::get().to(hello))
+
+        })
         .bind("0.0.0.0:8080")?
         .run()
         .await
---
+++ common/src/db.rs
@@ -0,0 +1,41 @@
+/// Initialize the Database connection pool and make available to all workspaces
+
+use config::Config;
+use serde::Deserialize;
+use mongodb::{Client, Database};
+
+#[derive(Debug, Deserialize)]
+struct DatabaseSettings {
+    mongodb: DatabaseCredentials,
+}
+
+#[derive(Debug, Deserialize)]
+struct DatabaseCredentials {
+    uri: String,
+    database: String,
+}
+
+// initialize environment variables here
+fn init_env_variables() -> DatabaseSettings {
+    let settings = Config::builder()
+        .add_source(config::File::with_name("settings_dev.toml"))
+        .add_source(config::Environment::with_prefix("APP"))
+        .build()
+        .expect("Failed to build configuration");
+
+    let db_settings: DatabaseSettings = settings
+        .try_deserialize()
+        .expect("Failed to deserialize configuration");
+
+    db_settings
+
+}
+
+pub async fn init_db() -> Database {
+    let settings = init_env_variables();
+    let client = Client::with_uri_str(settings.mongodb.uri)
+            .await
+            .expect("Failed to connect to mongodb");
+    let database = client.database(&settings.mongodb.database);
+    database
+}
